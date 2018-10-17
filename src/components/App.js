9f75844420d43dd05239dfa4f8d8668959f15b28
Mike Taylor <mctaylor17@gmail.com>
2018-10-17 10:53:50 -0700

    Adds favicons and moves head into component

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,18 @@ import React from "react";
 import Helmet from "react-helmet";
 import Main from "./Main";
 import Header from "./Header";
+import Head from "./Head";
 import AnchorPoint from "react-anchor-point";
 import styles from "./global.scss";
+
+
+//<link rel="icon" type="image/png" href="favicon.ico" sizes="16x16" />
 
 const App = () => {
   return (
     <div>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>MKTLR = Marketing + Web Development</title>
-      </Helmet>
-    
+      <Head/>    
       <Header />
-    
       <AnchorPoint ref={(reactAnchor) => { window.reactAnchor = reactAnchor}}>
         <Main />
       </AnchorPoint>
