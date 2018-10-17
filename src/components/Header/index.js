64ab6e809d96fd47d753a3ea18edde641df32ad2
Mike Taylor <mctaylor17@gmail.com>
2018-10-16 17:16:14 -0700

    Adds a fancy name to the header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "./styles.scss";
 
 const Header = () => (
   <header>
-    <nav>
+    <h1 className={styles.h1}>M<span className={styles.hidden}>I</span>K<span className={styles.hidden + " " + styles.gap}>E </span>T<span className={styles.hidden}>AY</span>L<span className={styles.hidden}>O</span>R</h1>
+    <nav className={styles.nav}>
       <ul>
         <li><Link to='/'>Home</Link></li>
-        <li><Link to='/child1'>Child1</Link></li>
+        <li><Link to='/dewordify'>Dewordify</Link></li>
         <li><Link to='/child2'>Child2</Link></li>
       </ul>
     </nav>
-    <h1>MKTLR</h1>
   </header>
 )
 
