28a76ed86d527f26cbec691c50d9216308eaae19
Mike Taylor <mctaylor17@gmail.com>
2018-10-19 13:34:51 -0700

    Adds header stylesheet

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import MKTLR from "./MKTLR";
 import NavBar from "./NavBar";
+import styles from "./styles.scss";
 
 class Header extends React.Component { 
   render() {
     return (
-      <header>
+      <header className={styles.header}>
         <MKTLR/>
         <NavBar/>
       </header>    
